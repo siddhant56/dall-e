66220a963f963e21f7ced39ca90fafc2f9b639fd
Siddhant Trivedi <siddhant56>
2023-04-08 14:59:26 +0530

    API HOST

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,10 +22,11 @@ const Home = () => {
         setLoading(true);
 
         try {
-          const response = await fetch('http://localhost:3000/api/v1/post',{
+          const response = await fetch('https://dall-e-cu49.onrender.com/api/v1/post',{
             method:'GET',
             headers:{
-              'Content-Type':'application/json'
+              'Content-Type':'application/json',
+              'Access-Control-Allow-Origin':'*',
             }
           })
 
